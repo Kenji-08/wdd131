c9723e5cf6303fbd2204d642dfbf9f5dd42395f1
Kenji-08 <123200723+Kenji-08@users.noreply.github.com>
2025-09-22 11:53:20 -0600

    ponder var

--- Ponder/week1/meta-css.js
+++ Ponder/week1/meta-css.js
@@ -5,7 +5,17 @@ let radius = 3;
 
 const one = 1;
 const two = '2';
-                    
+
+area = radius * radius * PI;
+
+console.log(area);
+
+let result = one + Number(two);
+console.log(result);
+
+result = one * two;
+console.log(result);
+
 
 let course = "CSE131"; //global scope
 if (true) {
@@ -15,4 +25,4 @@ if (true) {
 }
 console.log(course); //works fine, course is global
 console.log(student); //does not work, can't access a block variable outside the block
-                    
+

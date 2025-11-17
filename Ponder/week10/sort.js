3ec80b7e68bfd992b4b2cc6398821d601e1cd57c
Kenji-08 <123200723+Kenji-08@users.noreply.github.com>
2025-11-17 12:21:26 -0700

    sort ponder

---
+++ Ponder/week10/sort.js
@@ -0,0 +1,105 @@
+const nums = [12, 1, 55, 3, 6, 20];
+console.log(nums.sort(compareFn));
+
+const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
+console.log(simpleList.sort());
+
+let lowerList = simpleList.map(function(fruit){
+  return fruit.toLowerCase();
+})
+console.log(lowerList.sort());
+
+// ----------------------------------------------------------------------------------
+
+// filter
+let searchTerm = 'apples';
+
+let filterFruit = lowerList.filter(searchFruit);
+
+function searchFruit(fruit) {
+  return fruit.includes(searchTerm); // Find out if the value exists in the string
+}
+
+console.log(filterFruit);
+
+// ----------------------------------------------------------------------------------
+
+function compareFn(a,b) {
+  if (a.productName < b.productName) {
+    return -1;
+  } else if (a.productName > b.productName) {
+    return 1;
+  }
+ return 0;
+}
+
+const products = [
+  {
+    productName: "Wireless Mouse",
+    price: 29.99
+  },
+  {
+    productName: "Bluetooth Keyboard",
+    price: 49.99
+  },
+  {
+    productName: "Laptop Stand",
+    price: 39.99
+  }
+];
+
+console.log(products.sort(compareFn))
+
+function compareFn(a,b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+ return 0;
+}
+
+const animals = [
+  {
+    name: "Lion",
+    traits: ["brave", "strong", "fierce", "wild"]
+  },
+  {
+    name: "Elephant",
+    traits: ["large", "gentle", "smart", "wild"]
+  },
+  {
+    name: "Fox",
+    traits: ["sly", "quick", "clever", "wild"]
+  },
+  {
+    name: "Dog",
+    traits: ["loyal", "friendly", "playful", "cuddly"]
+  },
+  {
+    name: "Cat",
+    traits: ["quiet", "independent", "curious", "cuddly"]
+  }
+];
+
+let query = 'dog';
+
+let filterdList = animals.filter(searchList);
+
+function searchList(item) {
+  return item.name.toLowerCase().includes(query.toLowerCase());
+}
+
+console.log(filterdList);
+
+let queryTrait = 'strong';
+
+let filteredTraits = animals.filter(searchTraits);
+
+function searchTraits(item){
+  return item.traits.find(trait=>
+    trait.toLowerCase().includes(queryTrait.toLowerCase()))
+}
+
+console.log(filteredTraits);
+
